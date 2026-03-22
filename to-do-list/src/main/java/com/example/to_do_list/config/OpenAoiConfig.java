83a426bc481cf2e247dd8c70fe557e2d2bf4d129
AmanaArmel <121455023+AmanaArmel@users.noreply.github.com>
2026-03-22 11:31:51 +0000

    openApi update

--- to-do-list/src/main/java/com/example/to_do_list/config/OpenAoiConfig.java
+++ to-do-list/src/main/java/com/example/to_do_list/config/OpenAoiConfig.java
@@ -1,7 +1,7 @@
 package com.example.to_do_list.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement; // CLASSE, pas annotation
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
